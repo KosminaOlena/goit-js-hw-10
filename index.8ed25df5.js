e644d33a86932785702b60941740089ac1aa5d60
KosminaOlena <113665261+KosminaOlena@users.noreply.github.com>
2023-03-25 16:31:13 +0000

    Deploying to gh-pages from @ KosminaOlena/goit-js-hw-10@b9ffc0b255ef1e7fabcf0ac8b6096cff3c926c9b 🚀

---
+++ index.8ed25df5.js
@@ -0,0 +1,2 @@
+!function(){var e={search:document.querySelector("#search-box"),list:document.querySelector(".country-list"),info:document.querySelector(".country-info")};e.search.addEventListener("input",(function(o){console.log(e.search.value)}))}();
+//# sourceMappingURL=index.8ed25df5.js.map
